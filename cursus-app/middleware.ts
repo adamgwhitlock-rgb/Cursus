ad4b17bdae953ef700be9862aaab7735e2bf5bf6
adamgwhitlock-rgb <adamgwhitlock@googlemail.com>
2026-09-05 23:15:32 +0100

    Update middleware.ts

--- cursus-app/middleware.ts
+++ cursus-app/middleware.ts
@@ -1,10 +1,5 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware({
-  publishableKey: "pk_test_c3VpdGVkLWxhYnJhZG9yLTMzMzYuY2xlcmsuYWNjb3VudHMuZGV2",
-  secretKey: "sk_test_" + "tFpXjfZdr0I7S3gHpgDfuqbYpzPTNEp8Bu2wyQniNS",
-});
-
+export default clerkMiddleware();
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
